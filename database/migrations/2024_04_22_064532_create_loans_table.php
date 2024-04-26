dc76081feda0f1f732aa4aa5144ae97ac3e6aecb
gerrygeop <gerryja28@gmail.com>
2024-04-27 01:07:08 +0800

    added model Loan --mc

---
+++ database/migrations/2024_04_22_064532_create_loans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained('items')->cascadeOnDelete();
+            $table->integer('qty');
+            $table->date('borrowing_date');
+            $table->date('return_date');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};

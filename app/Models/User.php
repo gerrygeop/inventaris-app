f64e70b250d6add7d0bb3df4d155f792a4afd848
gerrygeop <gerryja28@gmail.com>
2024-04-29 18:34:00 +0800

    User resource

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,6 @@ class User extends Authenticatable
 
     public function roles(): BelongsToMany
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Role::class)->withTimestamps();
     }
 }

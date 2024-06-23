ace6173dffd2efbf9dd0cb1ea33dcdcf2e5ca390
gerrygeop <gerryja28@gmail.com>
2024-06-23 22:26:30 +0800

    feat(view): add new view file to Filament resources

--- app/Filament/Resources/LoanResource/Pages/ViewLoan.php
+++ app/Filament/Resources/LoanResource/Pages/ViewLoan.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\LoanResource\Pages;
 
 use App\Filament\Resources\LoanResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewLoan extends ViewRecord
